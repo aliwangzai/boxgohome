bcb6b4c21af87f65e148f0026ddb36a0b82c86b0
liujian890508 <liujian890508@gmail.com>
2014-09-11 13:42:25 +0800

    update win32 pro

--- Resources/fonts/fonts.bat
+++ Resources/fonts/fonts.bat
@@ -2,3 +2,4 @@
 set BMFONT_PATH=D:\AngelCode\BMFont
 set TEXT_PATH=H:\GameDev\Work\BoxGoHome\Resources\fonts
 %BMFONT_PATH%\bmfont.exe -t %TEXT_PATH%\fonts.txt -c %TEXT_PATH%\fonts.bmfc -o %TEXT_PATH%\fonts.fnt
+pause
