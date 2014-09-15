0915b4918684028a2b24a5b3991447094c179685
franklen <609008967@qq.com>
2014-09-15 20:07:23 +0800

    Game UI

--- Classes/GameUI.cpp
+++ Classes/GameUI.cpp
@@ -6,6 +6,12 @@
 #include "LevelState.h"
 #include "Utils.h"
 
+#include "cocostudio/CCSGUIReader.h"
+#include "ui/CocosGUI.h"
+#include "ui/UILayout.h"
+using namespace ui;
+
+
 #define SCORERECOARD_LEVEL(level) "user_score_" + std::to_string(level)
 
 GameUI::GameUI()
@@ -61,85 +67,123 @@ bool GameUI::initSound()
 
 bool GameUI::initMoreGame()
 {
-	TTFConfig ttfConfig("fonts/Marker Felt.ttf", 32);
-	auto moreGameLabel = Label::createWithTTF(ttfConfig, "More Game");
-	auto moreGameItem = MenuItemLabel::create(moreGameLabel, [=](Ref *pSender){
-		
-	});
-	moreGameItem->setColor(Color3B(0, 0, 0));
-	auto menu = Menu::create(moreGameItem, nullptr);
-	this->addChild(menu);
-	menu->setPosition(VisibleRect::rightBottom() + Point(-80, 20));
+// 	TTFConfig ttfConfig("fonts/Marker Felt.ttf", 32);
+// 	auto moreGameLabel = Label::createWithTTF(ttfConfig, "More Game");
+// 	auto moreGameItem = MenuItemLabel::create(moreGameLabel, [=](Ref *pSender){
+// 		
+// 	});
+// 	moreGameItem->setColor(Color3B(0, 0, 0));
+// 	auto menu = Menu::create(moreGameItem, nullptr);
+// 	this->addChild(menu);
+// 	menu->setPosition(VisibleRect::rightBottom() + Point(-80, 20));
 	return true;
 }
 
 bool GameUI::initMenu()
 {
-	TTFConfig ttfConfig("fonts/Marker Felt.ttf", 32);
-	auto restartLabel = Label::createWithTTF(ttfConfig, "Restart");
-	auto restartItem = MenuItemLabel::create(restartLabel, [=](Ref *pSender){
+	//TTFConfig ttfConfig("fonts/Marker Felt.ttf", 32);
+	///auto restartLabel = Label::createWithTTF(ttfConfig, "Restart");
+	auto restartItem = MenuItemImage::create("ui/btn_reset.png" , "ui/btn_reset.png" ,"ui/btn_reset.png", [=](Ref *pSender){
 		this->m_pGameWorld->restart();
 	});
-	restartItem->setColor(Color3B(0, 0, 0));
-	auto menuLabel = Label::createWithTTF(ttfConfig, "Menu");
-	auto menuItem = MenuItemLabel::create(menuLabel, [=](Ref *pSender){
+	restartItem->setOpacity(100);
+	//restartItem->setColor(Color3B(0, 0, 0));
+	//auto menuLabel = Label::createWithTTF(ttfConfig, "Menu");
+	auto menuItem = MenuItemImage::create("ui/btn_menu.png", "ui/btn_menu.png","ui/btn_menu.png", [=](Ref *pSender){
 		Director::getInstance()->replaceScene(MenuLayer::createScene());
 	});
-	menuItem->setColor(Color3B(0, 0, 0));
+	menuItem->setOpacity(100);
+	//menuItem->setColor(Color3B(0, 0, 0));
 	auto menu = Menu::create(restartItem, menuItem, nullptr);
 	this->addChild(menu);
 	menu->alignItemsHorizontallyWithPadding(20);
-	menu->setPosition(VisibleRect::rightTop() + Point(-100, -20));
+	menu->setPosition(VisibleRect::rightTop() + Point(-100, -40));
 	return true;
 }
 
 bool GameUI::initScoreUI()
 {
-	TTFConfig ttfConfig("fonts/Marker Felt.ttf", 25);
-	//--------------------------------------level-----------------------------------
-	auto levelNameLabel = Label::createWithTTF(ttfConfig, "Level:");
-	this->addChild(levelNameLabel);
-	levelNameLabel->setPosition(VisibleRect::leftTop() + Point(50, -30));
-	levelNameLabel->setColor(Color3B(0, 0, 0));
-
-	m_pLevelLabel = Label::createWithTTF(ttfConfig, "");
-	this->addChild(m_pLevelLabel);
-	m_pLevelLabel->setPosition(VisibleRect::leftTop() + Point(85, -30));
-	m_pLevelLabel->setColor(Color3B(0, 0, 0));
-
-	//--------------------------------------score-----------------------------------
-	auto scoreNameLabel = Label::createWithTTF(ttfConfig, "Score:");
-	this->addChild(scoreNameLabel);
-	scoreNameLabel->setPosition(VisibleRect::leftTop() + Point(150, -30));
-	scoreNameLabel->setColor(Color3B(0, 0, 0));
-
-	m_pScoreLabel = Label::createWithTTF(ttfConfig, "1");
-	this->addChild(m_pScoreLabel);
-	m_pScoreLabel->setPosition(VisibleRect::leftTop() + Point(215, -30));
-	m_pScoreLabel->setColor(Color3B(0, 0, 0));
+	//GameUI_UP.json
+	Layout* layout = static_cast<Layout*>(cocostudio::GUIReader::getInstance()->widgetFromJsonFile("ui/GameUI_UP.json"));
+	addChild(layout);
+	layout->setAnchorPoint(Vec2(0,1));
+	layout->setPosition(VisibleRect::leftTop());
 
 
-	//--------------------------------------bonus-----------------------------------
-	auto bonusNameLabel = Label::createWithTTF(ttfConfig, "Bonus:");
-	this->addChild(bonusNameLabel);
-	bonusNameLabel->setPosition(VisibleRect::leftTop() + Point(305, -30));
-	bonusNameLabel->setColor(Color3B(0, 0, 0));
+	ui::Text* label =  static_cast<ui::Text*>(layout->getChildByName("label_0_score"));
+	label->enableOutline(Color4B::BLACK , 1);
 
-	m_pBonusLabel = Label::createWithTTF(ttfConfig, "1");
-	this->addChild(m_pBonusLabel);
-	m_pBonusLabel->setPosition(VisibleRect::leftTop() + Point(365, -30));
-	m_pBonusLabel->setColor(Color3B(0, 0, 0));
+	label =   static_cast<ui::Text*>(layout->getChildByName("label_0_bonus"));
+	label->enableOutline(Color4B::BLACK , 1);
 
-	//--------------------------------------jumps-----------------------------------
-	auto jumpsNameLabel = Label::createWithTTF(ttfConfig, "Jumps:");
-	this->addChild(jumpsNameLabel);
-	jumpsNameLabel->setPosition(VisibleRect::leftTop() + Point(500, -30));
-	jumpsNameLabel->setColor(Color3B(0, 0, 0));
+	label =  static_cast<ui::Text*>(layout->getChildByName("label_0_jump"));
+	label->enableOutline(Color4B::BLACK , 1);
 
-	m_pJumpsLabel = Label::createWithTTF(ttfConfig, "5");
-	this->addChild(m_pJumpsLabel);
-	m_pJumpsLabel->setPosition(VisibleRect::leftTop() + Point(545, -30));
-	m_pJumpsLabel->setColor(Color3B(0, 0, 0));
+	//
+
+
+	m_pScoreLabel =  static_cast<ui::Text*>(layout->getChildByName("label_score"));
+	m_pScoreLabel->enableOutline(Color4B::BLACK , 1);
+
+	m_pBonusLabel =  static_cast<ui::Text*>(layout->getChildByName("label_bonus"));
+	m_pBonusLabel->enableOutline(Color4B::BLACK , 1);
+
+	m_pJumpsLabel =  static_cast<ui::Text*>(layout->getChildByName("label_jump"));
+	m_pJumpsLabel->enableOutline(Color4B::BLACK , 1);
+
+// 	TTFConfig ttfConfig("ui/grobold.ttf", 25);
+// 	//--------------------------------------level-----------------------------------
+// 	auto levelNameLabel = Label::createWithTTF(ttfConfig, "Level:");
+// 	this->addChild(levelNameLabel);
+// 	levelNameLabel->setPosition(VisibleRect::leftTop() + Point(50, -30));
+// 	levelNameLabel->setColor(Color3B(255,255, 255));
+// 	levelNameLabel->enableOutline(Color4B::BLACK , 1);
+// 
+// 	m_pLevelLabel = Label::createWithTTF(ttfConfig, "");
+// 	this->addChild(m_pLevelLabel);
+// 	m_pLevelLabel->setPosition(VisibleRect::leftTop() + Point(95, -30));
+// 	m_pLevelLabel->setColor(Color3B(255,255, 255));
+// 	m_pLevelLabel->enableOutline(Color4B::BLACK , 1);
+// 
+// 	//--------------------------------------score-----------------------------------
+// 	auto scoreNameLabel = Label::createWithTTF(ttfConfig, "Score:");
+// 	this->addChild(scoreNameLabel);
+// 	scoreNameLabel->setPosition(VisibleRect::leftTop() + Point(160, -30));
+// 	scoreNameLabel->setColor(Color3B(255,255, 255));
+// 	scoreNameLabel->enableOutline(Color4B::BLACK , 1);
+// 
+// 	m_pScoreLabel = Label::createWithTTF(ttfConfig, "1");
+// 	this->addChild(m_pScoreLabel);
+// 	m_pScoreLabel->setPosition(VisibleRect::leftTop() + Point(230, -30));
+// 	m_pScoreLabel->setColor(Color3B(255,255, 255));
+// 	m_pScoreLabel->enableOutline(Color4B::BLACK , 1);
+// 
+// 
+// 	//--------------------------------------bonus-----------------------------------
+// 	auto bonusNameLabel = Label::createWithTTF(ttfConfig, "Bonus:");
+// 	this->addChild(bonusNameLabel);
+// 	bonusNameLabel->setPosition(VisibleRect::leftTop() + Point(305, -30));
+// 	bonusNameLabel->setColor(Color3B(255,255, 255));
+// 	bonusNameLabel->enableOutline(Color4B::BLACK , 1);
+// 
+// 	m_pBonusLabel = Label::createWithTTF(ttfConfig, "1");
+// 	this->addChild(m_pBonusLabel);
+// 	m_pBonusLabel->setPosition(VisibleRect::leftTop() + Point(365, -30));
+// 	m_pBonusLabel->setColor(Color3B(255,255, 255));
+// 	m_pBonusLabel->enableOutline(Color4B::BLACK , 1);
+// 
+// 	//--------------------------------------jumps-----------------------------------
+// 	auto jumpsNameLabel = Label::createWithTTF(ttfConfig, "Jumps:");
+// 	this->addChild(jumpsNameLabel);
+// 	jumpsNameLabel->setPosition(VisibleRect::leftTop() + Point(500, -30));
+// 	jumpsNameLabel->setColor(Color3B(255,255, 255));
+// 	jumpsNameLabel->enableOutline(Color4B::BLACK , 1);
+// 
+// 	m_pJumpsLabel = Label::createWithTTF(ttfConfig, "5");
+// 	this->addChild(m_pJumpsLabel);
+// 	m_pJumpsLabel->setPosition(VisibleRect::leftTop() + Point(545, -30));
+// 	m_pJumpsLabel->setColor(Color3B(255,255, 255));
+// 	m_pJumpsLabel->enableOutline(Color4B::BLACK , 1);
 	return true;
 }
 
@@ -155,8 +199,8 @@ void GameUI::setDefaultValue()
 
 void GameUI::setLevel(int level)
 {
-	this->m_nLevel = level;
-	m_pLevelLabel->setString(std::to_string(level));
+	//this->m_nLevel = level;
+	//m_pLevelLabel->setString(std::to_string(level));
 }
 
 void GameUI::setScore(int score)
