dba2f4c5cd0359b75e636a4b64b2f1c5792ee97e
liujian890508 <liujian890508@gmail.com>
2014-10-27 11:28:04 +0800

    update

--- profile/icon/IconBuilder/IconBuilder/frmMain.cs
+++ profile/icon/IconBuilder/IconBuilder/frmMain.cs
@@ -73,7 +73,6 @@ namespace IconBuilder
                 flag = flag && GetThumbnail(txtPath.Text, path + "Icon@2x.png", ImageFormat.Png, 114);//Application Icon for iPhone (retina display)
                 flag = flag && GetThumbnail(txtPath.Text, path + "Icon-72@2x.png", ImageFormat.Png, 114);//Application Icon for the new iPad (retina display)
                 flag = flag && GetThumbnail(txtPath.Text, path + "Icon-Small-50@2x.png", ImageFormat.Png, 100);//Settings/Spotlight icon for iPad
-                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-72.png", ImageFormat.Png, 72);//Application Icon for the iPad
                 flag = flag && GetThumbnail(txtPath.Text, path + "Icon-Small@2x.png", ImageFormat.Png, 58);//Settings/Spotlight icon for iPhone (retina display)
                 flag = flag && GetThumbnail(txtPath.Text, path + "Icon.png", ImageFormat.Png, 57);//Application Icon icon for iPhone
                 flag = flag && GetThumbnail(txtPath.Text, path + "Icon-Small-50.png", ImageFormat.Png, 50);//Settings/Spotlight icon for iPad
@@ -81,10 +80,21 @@ namespace IconBuilder
 				flag = flag && GetThumbnail(txtPath.Text, path + "Icon-28.png", ImageFormat.Png, 28);//Settings/Spotlight icon for iPhone
 				flag = flag && GetThumbnail(txtPath.Text, path + "Icon-108.png", ImageFormat.Png, 108);//Settings/Spotlight icon for iPhone
 				flag = flag && GetThumbnail(txtPath.Text, path + "Icon-16.png", ImageFormat.Png, 16);//Settings/Spotlight icon for iPhone
-				flag = flag && GetThumbnail(txtPath.Text, path + "Icon-80.png", ImageFormat.Png, 80);//Settings/Spotlight icon for iPhone
 				flag = flag && GetThumbnail(txtPath.Text, path + "Icon-120.png", ImageFormat.Png, 120);//Settings/Spotlight icon for iPhone
 				flag = flag && GetThumbnail(txtPath.Text, path + "Icon-64.png", ImageFormat.Png, 64);//Settings/Spotlight icon for iPhone
 				flag = flag && GetThumbnail(txtPath.Text, path + "Icon-75.png", ImageFormat.Png, 75);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-29.png", ImageFormat.Png, 29);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-40.png", ImageFormat.Png, 40);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-50.png", ImageFormat.Png, 50);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-57.png", ImageFormat.Png, 57);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-58.png", ImageFormat.Png, 58);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-72.png", ImageFormat.Png, 72);//Application Icon for the iPad
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-76.png", ImageFormat.Png, 76);//Application Icon for the iPad
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-80.png", ImageFormat.Png, 80);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-100.png", ImageFormat.Png, 100);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-114.png", ImageFormat.Png, 114);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-144.png", ImageFormat.Png, 144);//Settings/Spotlight icon for iPhone
+                flag = flag && GetThumbnail(txtPath.Text, path + "Icon-152.png", ImageFormat.Png, 152);//Settings/Spotlight icon for iPhone
                 if (flag)
                 {
                     MessageBox.Show("ICON生成完毕！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
